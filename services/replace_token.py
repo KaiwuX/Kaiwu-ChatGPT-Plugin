55c4797efbe0acd3d1ba4e28e54a758dda95a2c3
nanli <linanenv@gmail.com>
2023-10-25 13:01:13 +0800

    fix none type

--- services/replace_token.py
+++ services/replace_token.py
@@ -4,18 +4,17 @@ import os
 # Input your OpenAI API key
 api_key = os.environ.get("OPENAI_API_TOKEN")
 
-# File path
-file_path = (
-    ".well-known/ai-plugin.json"  # Please replace your_file_name with your file name
-)
+if api_key is not None:
+    # File path
+    file_path = ".well-known/ai-plugin.json"  # Please replace your_file_name with your file name
 
-# Read JSON file
-with open(file_path, "r", encoding="utf-8") as f:
-    data = json.load(f)
+    # Read JSON file
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-# Replace API key
-data["auth"]["verification_tokens"]["openai"] = api_key
+    # Replace API key
+    data["auth"]["verification_tokens"]["openai"] = api_key
 
-# Write the modified data back to the JSON file
-with open(file_path, "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    # Write the modified data back to the JSON file
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
